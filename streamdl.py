0eb48ec7da5875d9287946bfe7fd0860bb151976
douchecanoe <mattwhelpley@gmail.com>
2020-10-13 14:08:31 -0600

    Update streamdl.py

--- streamdl.py
+++ streamdl.py
@@ -3,13 +3,13 @@ import csv
 from ffmpeg_streaming import Formats
 
 with open('<your list of m3u8s>.csv', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
+	data = list(csv.reader(csvfile))
 
 
 for x in range(len(data)):
-    filename = data[x][0]
+	filename = data[x][0]
 	streamlink = data[x][1]
 	print("Writing File: " + filename)
 	video = ffmpeg_streaming.input(streamlink)
-    stream = video.stream2file(Formats.h264())
+	stream = video.stream2file(Formats.h264())
 	stream.output('/mnt/d/Turning/' + filename + '.mp4')
